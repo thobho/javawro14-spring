6d30c4bc3f29f387449b421bc0bf0f9ebbf57472
tomasz.bar3@gmail.com <Niuteczka1!>
2019-06-01 14:37:42 +0200

    initial commit

---
+++ src/main/java/com/sda/javawro14/Javawro14Application.java
@@ -0,0 +1,13 @@
+package com.sda.javawro14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javawro14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javawro14Application.class, args);
+	}
+
+}
