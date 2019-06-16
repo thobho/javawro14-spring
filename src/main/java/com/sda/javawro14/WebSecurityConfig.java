6e9f1fccb2a12e4b59eeba591a6d645dc5020a7e
tomasz.bar3@gmail.com <Niuteczka1!>
2019-06-16 11:45:20 +0200

    Day 4 synchro

--- src/main/java/com/sda/javawro14/WebSecurityConfig.java
+++ src/main/java/com/sda/javawro14/WebSecurityConfig.java
@@ -1,17 +1,20 @@
 package com.sda.javawro14;
 
+import com.sda.javawro14.user.model.User;
 import com.sda.javawro14.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
 
+    @Autowired
     public WebSecurityConfig(UserService userService) {
         this.userService = userService;
     }
@@ -27,13 +31,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+
                 .authorizeRequests()
-                .antMatchers("/", "/logout").permitAll()
+                .antMatchers("/").permitAll()
+
                 .anyRequest().authenticated()
                 .and()
+
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
+
                 .and()
                 .logout()
                 .invalidateHttpSession(true)
@@ -44,14 +52,34 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
-        UserDetails[] users = userService.getAllUsers().stream()
-                .map(userDocument -> User.withDefaultPasswordEncoder()
-                        .username(userDocument.getName())
-                        .password(userDocument.getName())
-                        .roles("USER")
-                        .build())
+        List<com.sda.javawro14.user.model.User> allUsers = userService.getAllUsers();
+        Optional<User> admin = allUsers.stream()
+                .filter(user -> user.getName().equals("admin"))
+                .findAny();
+        if (!admin.isPresent()) {
+            User user =
+                    new User("admin", new Date());
+            userService.saveUser(user);
+        }
+        UserDetails[] users = allUsers.stream()
+                .map(this::createUser)
                 .toArray(UserDetails[]::new);
-
         return new InMemoryUserDetailsManager(users);
+    }
+
+    private UserDetails createUser(User userDocument) {
+        if (userDocument.getName().equals("admin")) {
+            return org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder()
+                    .username(userDocument.getName())
+                    .password(userDocument.getName())
+                    .roles("USER", "ADMIN")
+                    .build();
+        } else {
+            return org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder()
+                    .username(userDocument.getName())
+                    .password(userDocument.getName())
+                    .roles("USER")
+                    .build();
+        }
     }
 }
